ea2142913c033c074cb47c2d740dbb8a1500e7e0
Allan Wu <allan.wu6@gmail.com>
2021-03-12 23:42:09 +1100

    Problem Complete
    Use Greedy Method and iterate along the array updating the local max and global max sum.

--- Easy/problem_53.cs
+++ Easy/problem_53.cs
@@ -1,27 +1,30 @@
 /*
     Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 */
+
 public class Solution {
     public int MaxSubArray(int[] nums) {  
-        int[] currSum = new int[nums.Length - 1];
-        int[] seenSum = new int[nums.Length - 1];
+        int currSum;
+        int maxSum;
+        
+        currSum = maxSum = nums[0];
         
         if (nums.Length == 1) {
             return nums[0];
         }
         
-        currSum[0] = 
-        for (int i = 1; i < nums.Length - 1; i++) {
-            
+        for (int i = 1; i < nums.Length; i++) {
+            if (nums[i] > currSum + nums[i]) {
+                currSum = nums[i];
+            } else {
+                currSum = currSum + nums[i];
+            }
+
+            if (currSum > maxSum) {
+                maxSum = currSum;
+            }
         }
         
-        int sum = int32.MinValue;
-        for (int j = 0; j < seenSum.Length - 1; j++) {
-            if (seenSum[j] > sum) {
-                sum = seenSum[j];
-            }
-        } 
-        
-        return sum;
+        return maxSum;
     }
 }
